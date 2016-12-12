eb91b26d1e6f407ca58753f014fad751fb2330c0
Andrea Casino <andreacasino@Andreas-MacBook-Pro.local>
2016-12-12 19:52:09 +0000

    works very well now ! ^^ Added corner cases

--- objects.js
+++ objects.js
@@ -1,3 +1,7 @@
+/* Author: Andrea Casino*/
+/* Date: 12, December, 2016 */
+
+//Variable size grid object
 function makeGrid(gridSize) {
 
   this.gridSize = gridSize;
@@ -7,6 +11,7 @@ function makeGrid(gridSize) {
   this.bordersize=1;
   this.blockSize=floor(width/gridSize);
 
+  //initialises grid to be all 0s
   this.init = function(){
     for(i=0;i<this.gridSize*this.gridSize;i++){
 
@@ -19,25 +24,31 @@ function makeGrid(gridSize) {
   this.display = function() {
 
     counter=0;
-
+    //Displays every square
     for(i=0;i<this.gridSize;i++){
         for(j=0;j<this.gridSize;j++){
 
+          //Displays black background
           fill(0);
           quad(i*this.blockSize,j*this.blockSize,
             (i+1)*this.blockSize,j*this.blockSize,
             (i+1)*this.blockSize,(j+1)*this.blockSize,
             i*this.blockSize,(j+1)*this.blockSize);
+
+            //Checks if square is alive or dead
           if(this.grid[i*this.gridSize+j]==0){
             fill(150);
           }else {
             fill(250);
           }
+
+          //displays square;
           quad(i*this.blockSize+this.bordersize,j*this.blockSize+this.bordersize,
             (i+1)*this.blockSize-this.bordersize,j*this.blockSize+this.bordersize,
             (i+1)*this.blockSize-this.bordersize,(j+1)*this.blockSize-this.bordersize,
             i*this.blockSize+this.bordersize,(j+1)*this.blockSize-this.bordersize);
 
+            //Defines square area
             this.buttons.push(i*this.blockSize+this.bordersize,j*this.blockSize+this.bordersize,
               (i+1)*this.blockSize-this.bordersize,j*this.blockSize+this.bordersize,
               (i+1)*this.blockSize-this.bordersize,(j+1)*this.blockSize-this.bordersize,
@@ -54,10 +65,13 @@ function makeGrid(gridSize) {
     for(i=0;i<this.gridSize;i++){
         for(j=0;j<this.gridSize;j++){
 
+            //checks if mouse is within the square area
             if(mouseX > i*this.blockSize+this.bordersize &&
               mouseX < (i+1)*this.blockSize-this.bordersize &&
               mouseY > j*this.blockSize+this.bordersize &&
               mouseY < (j+1)*this.blockSize-this.bordersize){
+
+                //if it is, switch whatever color it is
                 if(this.grid[i*this.gridSize+j]==1){
 
                   this.grid[i*this.gridSize+j]=0;
@@ -77,6 +91,7 @@ function makeGrid(gridSize) {
 
   this.checkCells = function(){
 
+    //make copy of living and dead cells
     for(i=0;i<this.gridSize*this.gridSize;i++){
 
         this.grid2[i]=this.grid[i];
@@ -86,7 +101,66 @@ function makeGrid(gridSize) {
     for(i=0;i<this.gridSize;i++){
         for(j=0;j<this.gridSize;j++){
 
-          population=this.grid[(i-1)*this.gridSize+j-1]+
+          //calculates sorrounding population of cells
+          //different cases due to edges and corners
+
+          if(i==0 && j==0){
+            population=
+                      this.grid[(i)*this.gridSize+j+1]+
+                      this.grid[(i+1)*this.gridSize+j]+
+                      this.grid[(i+1)*this.gridSize+j+1];
+          }
+          else if(i== this.gridSize-1 && j==this.gridSize-1){
+            population=this.grid[(i-1)*this.gridSize+j-1]+
+                      this.grid[(i-1)*this.gridSize+j]+
+                      this.grid[(i)*this.gridSize+j-1];
+          }
+          else if(i== 0 && j==this.gridSize-1){
+            population=
+                      this.grid[(i)*this.gridSize+j-1]+
+                      this.grid[(i+1)*this.gridSize+j-1]+
+                      this.grid[(i+1)*this.gridSize+j];
+          }
+          else if(i== this.gridSize-1 && j==0){
+            population=
+                      this.grid[(i-1)*this.gridSize+j]+
+                      this.grid[(i-1)*this.gridSize+j+1]+
+                      this.grid[(i)*this.gridSize+j+1];
+          }
+          else if (i==0){
+            population=
+                      this.grid[(i)*this.gridSize+j-1]+
+                      this.grid[(i)*this.gridSize+j+1]+
+                      this.grid[(i+1)*this.gridSize+j-1]+
+                      this.grid[(i+1)*this.gridSize+j]+
+                      this.grid[(i+1)*this.gridSize+j+1];
+          }
+          else if (j==0){
+            population=
+                      this.grid[(i-1)*this.gridSize+j]+
+                      this.grid[(i-1)*this.gridSize+j+1]+
+                      this.grid[(i)*this.gridSize+j+1]+
+                      this.grid[(i+1)*this.gridSize+j]+
+                      this.grid[(i+1)*this.gridSize+j+1];
+          }
+
+          else if (i==this.gridSize-1){
+            population=this.grid[(i-1)*this.gridSize+j-1]+
+                      this.grid[(i-1)*this.gridSize+j]+
+                      this.grid[(i-1)*this.gridSize+j+1]+
+                      this.grid[(i)*this.gridSize+j-1]+
+                      this.grid[(i)*this.gridSize+j+1];
+          }
+          else if (j==this.gridSize-1){
+            population=this.grid[(i-1)*this.gridSize+j-1]+
+                      this.grid[(i-1)*this.gridSize+j]+
+                      this.grid[(i)*this.gridSize+j-1]+
+                      this.grid[(i+1)*this.gridSize+j-1]+
+                      this.grid[(i+1)*this.gridSize+j];
+;
+          }
+          else{
+            population=this.grid[(i-1)*this.gridSize+j-1]+
                       this.grid[(i-1)*this.gridSize+j]+
                       this.grid[(i-1)*this.gridSize+j+1]+
                       this.grid[(i)*this.gridSize+j-1]+
@@ -94,8 +168,9 @@ function makeGrid(gridSize) {
                       this.grid[(i+1)*this.gridSize+j-1]+
                       this.grid[(i+1)*this.gridSize+j]+
                       this.grid[(i+1)*this.gridSize+j+1];
-
-
+        }
+        //saves answer on copy so that the current changes don't affect population
+        //calculation
         if(population<2) {
           this.grid2[i*this.gridSize+j]=0;
         }
@@ -114,6 +189,7 @@ function makeGrid(gridSize) {
 
   }
 
+  //copy changes onto main grid
   for(i=0;i<this.gridSize*this.gridSize;i++){
 
       this.grid[i]=this.grid2[i];
--- sketch.js
+++ sketch.js
@@ -1,37 +1,49 @@
+/* Author: Andrea Casino*/
+/* Date: 12, December, 2016 */
+
+
 var mainGrid;
 var released = 1;
 var running = 0;
 
+//set up canvas
 function setup() {
   createCanvas(800,800);
-  mainGrid = new makeGrid(64);
+  mainGrid = new makeGrid(30);
   mainGrid.init();
 }
 
 function draw() {
+  //redraw background
   background(255);
 
+  //display grid
   mainGrid.display();
 
+  //check what box is clicked
   if(mouseIsPressed && released){
     mainGrid.checkClick();
     released = 0;
   }
 
+  //if system is running, turn it on
   if(running){
     mainGrid.checkCells();
   }
 
+  //FPS
   frameRate(10);
 
 }
 
+//so that multiple mouse clicks aren't registered
 function mouseReleased(){
 released = 1;
 }
 
+//Key to make it run
 function keyPressed() {
-  if (key == 'R' || key == 'r' ) {
+  if (key == ' ') {
     if(running==0){
       running=1;
     }
